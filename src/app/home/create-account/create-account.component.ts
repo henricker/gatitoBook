67f60f0cf3e9480f79203d7cd97b8fe69976d860
henricker <henriquevieira@alu.ufc.br>
2021-06-23 15:52:21 -0300

    Created reactive form

--- src/app/home/create-account/create-account.component.ts
+++ src/app/home/create-account/create-account.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ConfirmAndPasswordNotEquals } from './CustomValidators/ConfirmAndPasswordNotEquals.validator';
+import { lowerCaseValidator } from './CustomValidators/lowerCase.valitador';
+import { UserAndPasswordEqualsValidator } from './CustomValidators/UserAndPasswordEquals.validator';
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.component.html',
-  styleUrls: ['./create-account.component.scss']
+  styleUrls: ['./create-account.component.scss'],
 })
 export class CreateAccountComponent implements OnInit {
+  createAccountForm!: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.createAccountForm = this.formBuilder.group(
+      {
+        email: ['', [Validators.required, Validators.email]],
+        fullName: ['', [Validators.required]],
+        userName: ['', [Validators.required, lowerCaseValidator]],
+        password: ['', [Validators.required, Validators.min(6)]],
+        confirmPassword: ['', [Validators.required]],
+      },
+      {
+        validators: [
+          UserAndPasswordEqualsValidator,
+          ConfirmAndPasswordNotEquals,
+        ],
+      }
+    );
   }
 
+  register() {
+    console.log(this.createAccountForm.getRawValue());
+  }
 }
